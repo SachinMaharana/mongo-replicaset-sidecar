ae0a9ecc70228475d70cf58d1e8c59315215bcb6
Sachin Maharana <sachin.nicky@gmail.com>
2019-11-16 14:49:53 +0530

    added env

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SachinMaharana/mongo-replicaset-sidecar
 
 go 1.12
+
+require github.com/sirupsen/logrus v1.4.2
--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// Config holds the info of the binary
+type Config struct {
+	LabelSelector string
+	Namespace     string
+	Address       string
+	LabelAll      bool
+	LogLevel      logrus.Level
+}
+
+// Labeler is ...
+type Labeler struct {
+	Config *Config
+	Klient *kubernetes.Clientset
+}
+
+// New Returns an instance of Labeler
+func New() (*Labeler, error) {
+	config, err := getConfigFromEnvironment()
+	if err != nil {
+		return nil, err
+	}
+
+	kclient, err := getKubClientSet()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Labeler{
+		Config: config,
+		Klient: kclient,
+	}, nil
+}
+
+func getConfigFromEnvironment() (*Config, err) {
+	var label string
+	var ok string
+
+	if label, ok := os.LookupEnv("LABEL_SELECTOR"); !ok {
+		return nil, fmt.Errorf("LABEL_SELECTOR needs to be exported")
+	}
+
+}
 
 func main() {
-	fmt.Println("Start")
+
+	labeler, err := New()
+
 }
